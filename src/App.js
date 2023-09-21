9097606d920c827d04a853c7e9fcfd682b274551
Updesh95 <updesh.bhadoriya@telusinternational.com>
2023-09-22 02:24:06 +0900

    Basic components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import CategoryItem from "./components/category-item/category-item.component";
-import "./categories.styles.scss";
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
   const categories = [
     {
@@ -29,13 +29,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-containers">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -6,9 +6,11 @@ const CategoryItem = ({ category }) => {
     <div className="category-container">
       <div
         className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
       />
-      <div className="category-body-containers">
+      <div className="category-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
